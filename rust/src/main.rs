e0ae9c60ddddb0ff376b5cb47c07311d2dc46bca
Martin Indra <martin.indra@mgn.cz>
2019-07-04 23:33:59 +0200

    Rust: setup panic! logging

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,17 @@
 use irro::arduino::binary::Connection;
 use irro::{api, logging::IrroLogger, network};
-use log::info;
+use log::{error, info};
 use std::io;
+use std::panic;
 
 fn main() -> io::Result<()> {
     log::set_logger(&IrroLogger).expect("Could not initialize logger.");
     log::set_max_level(log::LevelFilter::Trace);
+
+    panic::set_hook(Box::new(|panic_info| {
+        error!("{}", panic_info);
+        println!("{}", panic_info);
+    }));
 
     info!("Starting Irro...");
 
